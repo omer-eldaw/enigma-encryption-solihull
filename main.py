ca6c3a93dfac797157b1aa86915d668edcd24c33
pwnedbytragic <omerdanis2@gmail.com>
2021-07-29 13:03:37 +0100

    reorganisation of files

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# asdf 
+class mainProgram():
+
+    def __init__(self, plugboard = {" " : " "}, rotor_1 = None, rotor_2 = None, rotor_3 = None):
+        pass
+
+if __name__ == "__main__":
+    plugboard = input("Enter the plugboard settings: ")
+    rotor_1 = int(input("Enter the value of rotor 1: "))
+    rotor_2 = int(input("Enter the value of rotor 2: "))
+    rotor_3 = int(input("Enter the value of rotor 3: "))
+    mainProgram()
